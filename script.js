8c21df21638af4f3a199d70825d246fef71896b9
nraghuraman <47099600+nraghuraman@users.noreply.github.com>
2019-06-30 21:41:08 -0400

    Add files via upload
    Improve variable and function names and add comments to the script

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var performAnimation = function(previousWasBigger) {
+/*
+ * Performs the animation of shrinking and expanding the navigation bar
+ */
+var performNavbarAnimation = function(previousWasBigger) {
 	var id = setInterval(animation, 5);
 	var newFont, newTop, newBottom;
 	[ newFont, newTop, newBottom ] = previousWasBigger ? [ 45, 5, 12 ] : [ 30, 0, 4.5 ];
@@ -16,6 +19,9 @@ var performAnimation = function(previousWasBigger) {
 	}
 }
 
+/*
+ * Sets the top button margins to the designated size
+ */
 var setButtonMargins = function(marginSize) {
 	var elements = document.getElementsByClassName("btn");
 	for (elem of elements) {
@@ -23,6 +29,9 @@ var setButtonMargins = function(marginSize) {
 	}
 }
 
+/*
+ * A closure which returns a function to be called whenever the window is scrolled
+ */
 window.onscroll = (function () {
 	var previousWasBigger = true;
 
@@ -31,14 +40,14 @@ window.onscroll = (function () {
 			document.getElementById("navbar").style.paddingTop = "0px"; // Sets back exactly to 0, considering float arithmetic issues
 			setButtonMargins("0px");
 			if (previousWasBigger) {
-				performAnimation(true);
+				performNavbarAnimation(true);
 				previousWasBigger = false;
 			}
 		} else {
 			document.getElementById("navbar").style.paddingTop = "5px";
 			setButtonMargins("23px");
 			if (!previousWasBigger) {
-				performAnimation(false);
+				performNavbarAnimation(false);
 				previousWasBigger = true;
 			}
 		}
